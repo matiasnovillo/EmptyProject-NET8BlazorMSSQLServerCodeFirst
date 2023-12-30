f40bc79bb302d4d17bd74a46319571f783de51bd
Matias Novillo <novillo.matias1@gmail.com>
2023-12-29 23:42:05 -0300

    Se sube una primera instancia de la app a un repositorio Github

---
+++ Components/Shared/StateContainer.cs
@@ -0,0 +1,9 @@
+﻿using EmptyProject.Entities;
+
+namespace EmptyProject.Components.Shared
+{
+    public class StateContainer
+    {
+        public User User { get; set; } = new User();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+using EmptyProject.Components;
+using EmptyProject.Components.Shared;
+using EmptyProject.Entities.Configuration;
+using EmptyProject.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddDbContext<EFCoreContext>();
+
+//Set access to repositories
+builder.Services.AddScoped<UserRepository>();
+
+//Set access to StateContainer to share data between Blazor components
+builder.Services.AddSingleton<StateContainer>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
